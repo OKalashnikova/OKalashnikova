56dbfd6be9b7a9296854f6a003ee2efaf1fe61a1
OKalashnikova <kalashnikovas@me.com>
2016-06-01 22:32:29 +0300

    Implement get in MainArray

--- src/MainArray.java
+++ src/MainArray.java
@@ -17,7 +17,7 @@ public class MainArray {
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
-        ARRAY_STORAGE.save(r4);
+ //       ARRAY_STORAGE.save(r4);
 
 //        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
 //        System.out.println("Size: " + ARRAY_STORAGE.size());
