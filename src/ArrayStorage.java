d85890393a1415fa35a30369677c5fe1ba859987
OKalashnikova <kalashnikovas@me.com>
2016-06-03 23:03:02 +0300

    1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,7 +5,7 @@ import java.util.Objects;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[100000];
+    Resume[] storage = new Resume[5];
     //int size;
 
 
