2cd16945ba899c8eb1e1da3ea7a9eae338a573ab
OKalashnikova <kalashnikovas@me.com>
2016-06-08 09:16:56 +0300

    save

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -10,23 +10,35 @@ public class ArrayStorage {
 
 
     void clear() {
-        int size=size();
+        int size = size();
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
-
     }
+
+
+//    void save(Resume r) {
+//        if (size() == storage.length) {
+//            System.out.println("К сожалению, место на сервере для хранения резюме закончилось");
+//        }
+//                storage[size()] = r;
+//        }
 
     void save(Resume r) {
         if (size() == storage.length) {
             System.out.println("К сожалению, место на сервере для хранения резюме закончилось");
         }
+        for (int i = 0; i < size(); i++) {
+            if (r.equals(storage[i].uuid)) {
+                System.out.println("drugoy id");
+                break;
+            }
+        }
         storage[size()] = r;
-
     }
 
     Resume get(String uuid) {
-        int size= size();
+        int size = size();
         Objects.requireNonNull(uuid, "uuis must not be null");
         for (int i = 0; i < size; i++) {
             if (storage[i] != null && uuid.equals(storage[i].uuid)) {
@@ -41,8 +53,8 @@ public class ArrayStorage {
         int size = size();
         for (int i = 0; i < size; i++) {
             if (storage[i] != null && uuid.equals(storage[i].uuid)) {
-                storage[i] = storage[size()-1];
-                storage[size()-1] = null;
+                storage[i] = storage[size() - 1];
+                storage[size() - 1] = null;
             }
         }
     }
@@ -51,7 +63,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int size= size();
+        int size = size();
         Resume[] result = new Resume[size()];
         int j = 0;
         for (int i = 0; i < size; i++) {
