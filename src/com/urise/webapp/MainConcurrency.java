7aeea66e034b1dd7519c91bd907e2e7dffbda2a3
OK <kalashnikovas@me.com>
2017-06-25 16:54:58 +0300

    Lesson 12 Threads

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -25,6 +25,8 @@ public class MainConcurrency {
         };
         thread0.start();
 
+//        System.out.println(thread0.isDaemon());
+
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -49,7 +51,7 @@ public class MainConcurrency {
             });
             thread.start();
             threads.add(thread);
-           // thread.join(); // sozdali potok, ego zapustili, pri pomoschi "join()" zhdem, poka on ispolnitsa ves
+            // thread.join(); // sozdali potok, ego zapustili, pri pomoschi "join()" zhdem, poka on ispolnitsa ves
         }
 
         threads.forEach(t -> {
@@ -61,6 +63,30 @@ public class MainConcurrency {
         });
         System.out.println("counter = " + counter);
         //LazySingleton.getInstance();
+
+
+        final String lock1 = "lock1";
+        final String lock2 = "lock2";
+        deadLock(lock1, lock2);
+        deadLock(lock2, lock1);
+    }
+
+    private static void deadLock(Object lock1, Object lock2){
+        new Thread(() ->{
+            System.out.println("Waiting " +lock1);
+            synchronized (lock1){
+                System.out.println("Holding " + lock1);
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Waiting " +lock2);
+                synchronized (lock2){
+                    System.out.println("Holding " + lock2);
+                }
+            }
+        }).start();
     }
 
     //chtobi zahodil tolko odin potok v metod: synchronized
