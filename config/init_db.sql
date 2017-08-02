2aeefe167d606663612578bc96febfa3bc2cdf5a
OK <kalashnikovas@me.com>
2017-08-03 00:53:15 +0300

    Lesson 13_create data base resumes SQL

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,13 +1,23 @@
-CREATE TABLE contact
-(
-  id INTEGER DEFAULT nextval('contact_id_seq'::regclass) PRIMARY KEY NOT NULL,
-  type TEXT NOT NULL,
-  value TEXT NOT NULL,
-  resume_uuid CHAR(36) NOT NULL,
-  CONSTRAINT contact_resume_uuid_fk FOREIGN KEY (resume_uuid) REFERENCES resume (uuid) ON DELETE CASCADE
-);
 CREATE TABLE resume
 (
-  uuid CHAR(36) PRIMARY KEY NOT NULL,
-  full_name TEXT
+  uuid      CHAR(36) NOT NULL
+    CONSTRAINT resume_pkey
+    PRIMARY KEY,
+  full_name TEXT     NOT NULL
 );
+
+CREATE TABLE contact
+(
+  id          SERIAL   NOT NULL
+    CONSTRAINT contact_pkey
+    PRIMARY KEY,
+  resume_uuid CHAR(36) NOT NULL
+    REFERENCES resume (uuid)
+    ON DELETE CASCADE,
+  type        TEXT     NOT NULL,
+  value       TEXT     NOT NULL
+
+);
+
+CREATE UNIQUE INDEX contact_uuid_type_index
+  ON contact (resume_uuid, type);
